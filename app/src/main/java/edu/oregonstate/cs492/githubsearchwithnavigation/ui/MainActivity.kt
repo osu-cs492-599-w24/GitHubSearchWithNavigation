0cb0ee967a40bbb4af0a447d71d551a9e8b16921
Rob Hess <hessro@oregonstate.edu>
2024-02-27 16:09:40 -0800

    Add code from 2/27 lecture.

--- app/src/main/java/edu/oregonstate/cs492/githubsearchwithnavigation/ui/MainActivity.kt
+++ app/src/main/java/edu/oregonstate/cs492/githubsearchwithnavigation/ui/MainActivity.kt
@@ -2,11 +2,36 @@ package edu.oregonstate.cs492.githubsearchwithnavigation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
+import com.google.android.material.appbar.MaterialToolbar
 import edu.oregonstate.cs492.githubsearchwithnavigation.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+        val appBar: MaterialToolbar = findViewById(R.id.top_app_bar)
+        setSupportActionBar(appBar)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 }
